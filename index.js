7d553b936c0c48c5cbb72a6878c58b5f1e02e540
Rizwanul Abidin Asim <b00099449@aus.edu>
2026-08-09 15:03:12 +0400

    Stage 2: read endpoints with 404

--- index.js
+++ index.js
@@ -2,12 +2,33 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let tasks= [
+    {id: 1, title : "task1", done : false},
+    {id: 2, title : "task2", done : false},
+    {id: 3, title : "task3", done : true}
+]
+
 app.get('/', (req, res) => {
   res.json({ "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] })
 });
 
 app.get('/health', (req, res) => {
   res.json({ "status" : "ok"})
+});
+
+app.get('/tasks', (req, res) => {
+  res.json(tasks)
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const task = tasks.find(t => t.id === id);
+
+  if(!task){
+    return res.status(404).json({error: `Task ${id} not found`});
+  }
+
+  res.json(task);
 });
 
 app.listen(port, () => {
